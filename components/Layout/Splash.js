db4e0d7374d1de150b8bb3b1bc17522b4142de8d
Ara <araoshinsky@gmail.com>
2018-12-22 12:32:16 -0800

    Add splash background files

--- components/Layout/Splash.js
+++ components/Layout/Splash.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Splash = () => (
   <div className="splash">
-    <div className="splash-top">
+    {/* <div className="splash-top"> */}
+    <div className="splash-background">
       <div className="splash-heading">
-      COUNTING THE WAYS CITY ACTIONS IMPACT PORTLANDERS EXPERIENCING HOUSELESSNESS
+        COUNTING THE WAYS CITY ACTIONS IMPACT PORTLANDERS EXPERIENCING HOUSELESSNESS
       </div>
     </div>
 
-    <div
+    {/* </div> */}
+
+    {/* <div
       className="splash-banner"
-    />
+    /> */}
   </div>
 );
 
